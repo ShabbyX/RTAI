91f0000fed71459999dd82973297f5f5920b759d
Shahbaz Youssefi <ShabbyX@gmail.com>
2013-12-12 10:53:05 +0100

    Update to magma Dec-12-2013
    Signed-off-by: Shahbaz Youssefi <ShabbyX@gmail.com>

--- base/include/rtai_math.h
+++ base/include/rtai_math.h
@@ -1,3 +1,331 @@
+#if 1
+
+/*
+ * Copyright (C) 2013 Paolo Mantegazza <mantegazza@aero.polimi.it>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+#ifndef _RTAI_MATH_H
+#define _RTAI_MATH_H
+
+//#define CONFIG_RTAI_GLIBC
+//#define CONFIG_RTAI_NEWLIB
+//#define CONFIG_RTAI_UCLIBC
+
+//#define CONFIG_RTAI_KCOMPLEX
+
+#ifdef __KERNEL__
+
+#include <rtai_schedcore.h>
+
+#define kerrno (_rt_whoami()->kerrno)
+
+char *d2str(double d, int dgt, char *str);
+
+#endif 
+
+/**************** BEGIN MACROS TAKEN AND ADAPTED FROM NEWLIB ****************/
+
+#ifndef HUGE_VAL
+#define HUGE_VAL (__builtin_huge_val())
+#endif
+
+#ifndef HUGE_VALF
+#define HUGE_VALF (__builtin_huge_valf())
+#endif
+
+#ifndef HUGE_VALL
+#define HUGE_VALL (__builtin_huge_vall())
+#endif
+
+#ifndef INFINITY
+#define INFINITY (__builtin_inff())
+#endif
+
+#ifndef NAN
+#define NAN (__builtin_nanf(""))
+#endif
+
+#define FP_NAN       0
+#define FP_INFINITE  1
+#define FP_ZERO      2
+#define FP_SUBNORMAL 3
+#define FP_NORMAL    4
+
+int __fpclassify(double x);
+int __fpclassifyf(float x);
+int __signbit(double x);
+int __signbitf(float x);
+
+#if CONFIG_RTAI_MATH_LIBM_TO_USE == 1
+int __fpclassifyd(double x);
+#define __fpclassify __fpclassifyd
+int __signbitd(double x);
+#define __signbit __signbitd
+#endif
+
+#define fpclassify(__x) \
+	((sizeof(__x) == sizeof(float))  ? __fpclassifyf(__x) : \
+	__fpclassify(__x))
+
+#ifndef isfinite
+#define isfinite(__y) \
+	(__extension__ ({int __cy = fpclassify(__y); \
+                           __cy != FP_INFINITE && __cy != FP_NAN;}))
+#endif
+
+#ifndef isinf
+#define isinf(y) (fpclassify(y) == FP_INFINITE)
+#endif
+
+#ifndef isnan
+#define isnan(y) (fpclassify(y) == FP_NAN)
+#endif
+
+#define isnormal(y) (fpclassify(y) == FP_NORMAL)
+#define signbit(__x) \
+	((sizeof(__x) == sizeof(float))  ?  __signbitf(__x) : \
+		__signbit(__x))
+
+#define isunordered(a, b) \
+          (__extension__ ({__typeof__(a) __a = (a); __typeof__(b) __b = (b); \
+                           fpclassify(__a) == FP_NAN || fpclassify(__b) == FP_NAN;}))
+
+#define isgreater(x, y) \
+          (__extension__ ({__typeof__(x) __x = (x); __typeof__(y) __y = (y); \
+                           !isunordered(__x,__y) && (__x > __y);}))
+
+#define isgreaterequal(x, y) \
+          (__extension__ ({__typeof__(x) __x = (x); __typeof__(y) __y = (y); \
+                           !isunordered(__x,__y) && (__x >= __y);}))
+
+#define isless(x, y) \
+          (__extension__ ({__typeof__(x) __x = (x); __typeof__(y) __y = (y); \
+                           !isunordered(__x,__y) && (__x < __y);}))
+
+#define islessequal(x, y) \
+          (__extension__ ({__typeof__(x) __x = (x); __typeof__(y) __y = (y); \
+                           !isunordered(__x,__y) && (__x <= __y);}))
+
+#define islessgreater(x, y) \
+          (__extension__ ({__typeof__(x) __x = (x); __typeof__(y) __y = (y); \
+                           !isunordered(__x,__y) && (__x < __y || __x > __y);}))
+
+/***************** END MACROS TAKEN AND ADAPTED FROM NEWLIB *****************/
+
+#define fpkerr(x) (isnan(x) || isinf(x))
+
+int matherr(void);
+
+double acos(double x);
+float acosf(float x);
+double acosh(double x);
+float acoshf(float x);
+double asin(double x);
+float asinf(float x);
+double asinh(double x);
+float asinhf(float x);
+double atan(double x);
+float atanf(float x);
+double atan2(double y, double x);
+float atan2f(float y, float x);
+double atanh(double x);
+float atanhf(float x);
+double j0(double x);
+float j0f(float x);
+double j1(double x);
+float j1f(float x);
+double jn(int n, double x);
+float jnf(int n, float x);
+double y0(double x);
+float y0f(float x);
+double y1(double x);
+float y1f(float x);
+double yn(int n, double x);
+float ynf(int n, float x);
+double cbrt(double x);
+float  cbrtf(float x);
+double copysign (double x, double y);
+float copysignf (float x, float y);
+double cosh(double x);
+float coshf(float x);
+double erf(double x);
+float erff(float x);
+double erfc(double x);
+float erfcf(float x);
+double exp(double x);
+float expf(float x);
+double exp2(double x);
+float exp2f(float x);
+double expm1(double x);
+float expm1f(float x);
+double fabs(double x);
+float fabsf(float x);
+double fdim(double x, double y);
+float fdimf(float x, float y);
+double floor(double x);
+float floorf(float x);
+double ceil(double x);
+float ceilf(float x);
+double fma(double x, double y, double z);
+float fmaf(float x, float y, float z);
+double fmax(double x, double y);
+float fmaxf(float x, float y);
+double fmod(double x, double y);
+float fmodf(float x, float y);
+double frexp(double val, int *exp);
+float frexpf(float val, int *exp);
+double gamma(double x);
+float gammaf(float x);
+double lgamma(double x);
+float lgammaf(float x);
+double gamma_r(double x, int *signgamp);
+float gammaf_r(float x, int *signgamp);
+double lgamma_r(double x, int *signgamp);
+float lgammaf_r(float x, int *signgamp);
+double tgamma(double x);
+float tgammaf(float x);
+double hypot(double x, double y);
+float hypotf(float x, float y);
+int ilogb(double val);
+int ilogbf(float val);
+double infinity(void);
+float infinityf(void);
+double ldexp(double val, int exp);
+float ldexpf(float val, int exp);
+double log(double x);
+float logf(float x);
+double log10(double x);
+float log10f(float x);
+double log1p(double x);
+float log1pf(float x);
+double log2(double x);
+float log2f(float x);
+double logb(double x);
+float logbf(float x);
+long int lrint(double x);
+long int lrintf(float x);
+long long int llrint(double x);
+long long int llrintf(float x);
+long int lround(double x);
+long int lroundf(float x);
+long long int llround(double x);
+long long int llroundf(float x);
+double modf(double val, double *ipart);
+float modff(float val, float *ipart);
+double nan(const char *);
+float nanf(const char *);
+double nearbyint(double x);
+float nearbyintf(float x);
+double nextafter(double val, double dir);
+float nextafterf(float val, float dir);
+double pow(double x, double y);
+float powf(float x, float y);
+double remainder(double x, double y);
+float remainderf(float x, float y);
+double remquo(double x, double y, int *quo);
+float remquof(float x, float y, int *quo);
+double rint(double x);
+float rintf(float x);
+double round(double x);
+float roundf(float x);
+double scalbn(double x, int n);
+float scalbnf(float x, int n);
+double scalbln(double x, long int n);
+float scalblnf(float x, long int n);
+double sin(double x);
+float  sinf(float x);
+double cos(double x);
+float cosf(float x);
+double sinh(double x);
+float  sinhf(float x);
+double sqrt(double x);
+float  sqrtf(float x);
+double tan(double x);
+float tanf(float x);
+double tanh(double x);
+float tanhf(float x);
+double trunc(double x);
+float truncf(float x);
+
+#ifdef CONFIG_RTAI_MATH_KCOMPLEX
+
+#define complex _Complex
+#define _Complex_I (__extension__ 1.0iF)
+#undef I
+#define I _Complex_I
+
+char *cd2str(complex double d, int dgt, char *str);
+
+#define cfpkerr(x) (fpkerr(__real__ x) || fpkerr(__imag__ x))
+
+double cabs(double _Complex z);
+float cabsf(float _Complex z);
+asmlinkage double _Complex cacos(double _Complex z);
+asmlinkage float _Complex cacosf(float _Complex z);
+asmlinkage double _Complex cacosh(double _Complex z);
+asmlinkage float _Complex cacoshf(float _Complex z);
+double carg(double _Complex z);
+float cargf(float _Complex z);
+asmlinkage double _Complex casin(double _Complex z);
+asmlinkage float _Complex casinf(float _Complex z);
+asmlinkage double _Complex casinh(double _Complex z);
+asmlinkage float _Complex casinhf(float _Complex z);
+asmlinkage double _Complex catan(double _Complex z);
+asmlinkage float _Complex catanf(float _Complex z);
+asmlinkage double _Complex catanh(double _Complex z);
+asmlinkage float _Complex catanhf(float _Complex z);
+asmlinkage double _Complex ccos(double _Complex z);
+asmlinkage float _Complex ccosf(float _Complex z);
+asmlinkage double _Complex ccosh(double _Complex z);
+asmlinkage float _Complex ccoshf(float _Complex z);
+asmlinkage double _Complex cexp(double _Complex z);
+asmlinkage float _Complex cexpf(float _Complex z);
+double cimag(double _Complex z);
+float cimagf(float _Complex z);
+asmlinkage double _Complex clog(double _Complex z);
+asmlinkage float _Complex clogf(float _Complex z);
+asmlinkage double _Complex clog10(double _Complex z);
+asmlinkage float _Complex clog10f(float _Complex z);
+asmlinkage double _Complex conj(double _Complex z);
+asmlinkage float _Complex conjf(float _Complex z);
+asmlinkage double _Complex cpow(double _Complex x, double _Complex y);
+asmlinkage float _Complex cpowf(float _Complex x, float _Complex y);
+asmlinkage double _Complex cproj(double _Complex z);
+asmlinkage float _Complex cprojf(float _Complex z);
+double creal(double _Complex z);
+float crealf(float _Complex z);
+asmlinkage double _Complex csin(double _Complex z);
+asmlinkage float _Complex csinf(float _Complex z);
+asmlinkage double _Complex csinh(double _Complex z);
+asmlinkage float _Complex csinhf(float _Complex z);
+asmlinkage double _Complex csqrt(double _Complex z);
+asmlinkage float _Complex csqrtf(float _Complex z);
+asmlinkage double _Complex ctan(double _Complex z);
+asmlinkage float _Complex ctanf(float _Complex z);
+asmlinkage double _Complex ctanh(double _Complex z);
+asmlinkage float _Complex ctanhf(float _Complex z);
+
+#endif
+
+#endif /* !_RTAI_MATH_H */
+
+#else
+
 /*
  * Declarations for math functions.
  * Copyright (C) 1991,92,93,95,96,97,98,99,2001 Free Software Foundation, Inc.
@@ -460,3 +788,5 @@ long int rinttol(double x);
 long int roundtol(double x);
 
 #endif /* !_RTAI_MATH_H  */
+
+#endif
--- base/math/libm.c
+++ base/math/libm.c
@@ -1,3 +1,530 @@
+#if 1
+
+/*
+ *  rtai/libm/libm.c - module wrapper for SunSoft/FreeBSD/MacOX/uclibc libm
+ *  RTAI - Real-Time Application Interface
+ *  Copyright (C) 2001 David A. Schleef <ds@schleef.org>
+ * 
+ * Dave's idea modified (2013) by Paolo Mantegazza <mantegazza@aero.polimi.it>, 
+ * so to use just the standard GPLed glibc, with the added possibility of 
+ * calling both the float and double version of glibc-libm functions.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA, 02111-1307, USA.
+*/
+
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+#include "rtai_math.h"
+
+MODULE_LICENSE("GPL");
+
+/***** Begin of entries needed by glibc-libm *****/
+
+int stderr = 2;
+
+#define kerrno_adr (&kerrno)
+
+int *__getreent(void)
+{
+	return kerrno_adr;
+}
+
+int *_impure_ptr(void)
+{
+	return kerrno_adr;
+}
+
+int *__errno(void)
+{
+	return kerrno_adr;
+}
+
+int *__errno_location(void)
+{
+	return kerrno_adr;
+}
+
+void __assert_fail(const char *assertion, const char *file, int line, const char *function)
+{
+	printk("An '__assert_fail' assertion has been called.\n");
+}
+
+int fputs(const char *s, void *stream)
+{ 
+	return printk("%s\n", s);
+}
+
+#define generic_echo(buf, size) \
+do { \
+	char str[size + 1]; \
+	memcpy(str, buf, size); \
+	str[size] = 0; \
+	return printk("%s\n", str); \
+} while (0)
+
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	generic_echo(ptr, size*nmemb);
+}
+
+ssize_t write(int fildes, const void *buf, size_t nbytes)
+{
+	generic_echo(buf, nbytes);
+}
+
+/***** End of entries needed by glibc-libm *****/
+
+char *d2str(double d, int dgt, char *str)
+{
+	const int MAXDGT = 17;
+	int e, i;
+	unsigned long long l;
+	double p;
+
+	if (d < 0) {
+		d = -d;
+		str[0] = '-';
+	} else {
+		str[0] = '+';
+	}
+	str[1] = '0';
+	str[2] = '.';
+	i = fpclassify(d);
+	if (i == FP_ZERO || i == FP_SUBNORMAL) {
+		memset(&str[3], '0', dgt);
+		strcpy(&str[dgt + 3], "e+00");
+		return str;
+	}
+	if (i == FP_NAN) {
+		strcpy(str, "NaN");
+		return str;
+	}
+	if (i == FP_INFINITE) {
+		strcpy(str, "Inf");
+		return str;
+	}
+	if (dgt <= 0) {
+		dgt = 1;
+	} else if (dgt > MAXDGT) {
+		dgt = MAXDGT;
+	}
+	e = log10(d);
+	p = pow(10, MAXDGT - e);
+	l = d*p + 0.5*pow(10, MAXDGT - dgt + (d >= 1));
+	sprintf(&str[3], "%llu", l);
+	i = dgt + 3;
+	str[i] = 'e';
+	if (e < 0) {
+		e = -e;
+		str[i + 1] = '-';
+	} else {
+		str[i + 1] = '+';
+	}
+	i = i + sprintf(&str[i + 2], "%d", e + (l/p >= 1));
+	str[i + 2] = 0;
+	return str;
+}
+EXPORT_SYMBOL(d2str);
+
+int signgam;
+
+#if CONFIG_RTAI_MATH_LIBM_TO_USE == 1
+#include "export_newlib.h"
+char using[7] = "NEWLIB";
+#endif
+#if CONFIG_RTAI_MATH_LIBM_TO_USE == 2
+#include "export_uclibc.h"
+char using[7] = "UCLIBC";
+#endif
+#if CONFIG_RTAI_MATH_LIBM_TO_USE == 3
+#include "export_glibc.h"
+char using[7] = "GLIBC";
+#endif
+
+int __rtai_math_init(void)
+{
+	printk(KERN_INFO "RTAI[math]: loaded, using %s.\n", using);
+	return 0;
+}
+
+void __rtai_math_exit(void)
+{
+	printk(KERN_INFO "RTAI[math]: unloaded.\n");
+}
+
+module_init(__rtai_math_init);
+module_exit(__rtai_math_exit);
+  
+EXPORT_SYMBOL(__fpclassify);
+EXPORT_SYMBOL(__fpclassifyf);
+EXPORT_SYMBOL(__signbit);
+EXPORT_SYMBOL(__signbitf);
+
+#if defined(CONFIG_RTAI_MATH_KCOMPLEX) && (defined(_RTAI_EXPORT_GLIBC_H) || defined(_RTAI_EXPORT_NEWLIB_H))
+
+char *cd2str(complex double cd, int dgt, char *str)
+{
+	int i;
+	d2str(__real__ cd, dgt, str);
+	i = strlen(str);
+	str[i] = ' ';
+#if 0
+	d2str(__imag__ cd, dgt, &str[i + 4]);
+	str[i + 1] = str[i + 4];
+	str[i + 2] = ' ';
+	str[i + 3] = 'j';
+	str[i + 4] = '*';
+#else
+	d2str(__imag__ cd, dgt, &str[i + 1]);
+	str[i = strlen(str)] = 'j';
+	str[i + 1] = 0;
+#endif
+	return str;
+}
+EXPORT_SYMBOL(cd2str);
+
+#if CONFIG_RTAI_MATH_LIBM_TO_USE == 3
+// Hopefully a provisional fix for glibc only. Till it is understood
+// why a plain call of glibc cpow and cpowf does not work
+asmlinkage double _Complex __cexp(double _Complex x);
+asmlinkage double _Complex __clog(double _Complex x);
+asmlinkage double _Complex cpow(double _Complex x, double _Complex y)
+{
+	return __cexp(y*__clog(x));
+}
+asmlinkage float _Complex __cexpf(float _Complex x);
+asmlinkage float _Complex __clogf(float _Complex x);
+asmlinkage float _Complex cpowf(float _Complex x, float _Complex y)
+{
+	return __cexpf(y*__clogf(x));
+}
+#endif
+
+#if CONFIG_RTAI_MATH_LIBM_TO_USE == 1
+asmlinkage double _Complex clog(double _Complex x);
+asmlinkage double _Complex clog10(double _Complex x)
+{
+	const double one_over_lnof10 = 0.4342944819032518276454794132;
+	return clog(x)*one_over_lnof10;
+}
+asmlinkage float _Complex clogf(float _Complex x);
+asmlinkage float _Complex clog10f(float _Complex x)
+{
+	const float one_over_lnof10 = 0.4342944819032518276454794132;
+	return clogf(x)*one_over_lnof10;
+}
+#endif
+
+double strtod(void) { return 0.0; }
+
+float strtof(void)  { return 0.0; }
+
+EXPORT_SYMBOL(cabs);
+EXPORT_SYMBOL(cabsf);
+EXPORT_SYMBOL(cacos);
+EXPORT_SYMBOL(cacosf);
+EXPORT_SYMBOL(cacosh);
+EXPORT_SYMBOL(cacoshf);
+EXPORT_SYMBOL(carg);
+EXPORT_SYMBOL(cargf);
+EXPORT_SYMBOL(casin);
+EXPORT_SYMBOL(casinf);
+EXPORT_SYMBOL(casinh);
+EXPORT_SYMBOL(casinhf);
+EXPORT_SYMBOL(catan);
+EXPORT_SYMBOL(catanf);
+EXPORT_SYMBOL(catanh);
+EXPORT_SYMBOL(catanhf);
+EXPORT_SYMBOL(ccos);
+EXPORT_SYMBOL(ccosf);
+EXPORT_SYMBOL(ccosh);
+EXPORT_SYMBOL(ccoshf);
+EXPORT_SYMBOL(cexp);
+EXPORT_SYMBOL(cexpf);
+EXPORT_SYMBOL(cimag);
+EXPORT_SYMBOL(cimagf);
+EXPORT_SYMBOL(clog);
+EXPORT_SYMBOL(clogf);
+EXPORT_SYMBOL(clog10);
+EXPORT_SYMBOL(clog10f);
+EXPORT_SYMBOL(conj);
+EXPORT_SYMBOL(conjf);
+EXPORT_SYMBOL(cpow);
+EXPORT_SYMBOL(cpowf);
+EXPORT_SYMBOL(cproj);
+EXPORT_SYMBOL(cprojf);
+EXPORT_SYMBOL(creal);
+EXPORT_SYMBOL(crealf);
+EXPORT_SYMBOL(csin);
+EXPORT_SYMBOL(csinf);
+EXPORT_SYMBOL(csinh);
+EXPORT_SYMBOL(csinhf);
+EXPORT_SYMBOL(csqrt);
+EXPORT_SYMBOL(csqrtf);
+EXPORT_SYMBOL(ctan);
+EXPORT_SYMBOL(ctanf);
+EXPORT_SYMBOL(ctanh);
+EXPORT_SYMBOL(ctanhf);
+
+#if 1
+
+double _Complex __muldc3(double a, double b, double c, double d)
+{
+	double _Complex z;
+	__real__ z = a*c - b*d; 
+	__imag__ z = a*d + b*c; 
+	return z;
+}
+
+float _Complex __mulsc3(float a, float b, float c, float d)
+{
+	float _Complex z;
+	__real__ z = a*c - b*d; 
+	__imag__ z = a*d + b*c; 
+	return z;
+}
+
+double _Complex __divdc3(double a, double b, double c, double d)
+{
+	double _Complex z;
+	double dn = c*c + d*d;
+	__real__ z = (a*c + b*d)/dn; 
+	__imag__ z = (-a*d + b*c)/dn; 
+	return z;
+}
+
+float _Complex __divsc3(float a, float b, float c, float d)
+{
+	float _Complex z;
+	float dn = c*c + d*d;
+	__real__ z = (a*c + b*d)/dn; 
+	__imag__ z = (-a*d + b*c)/dn; 
+	return z;
+}
+
+#else
+
+/*
+ *                     The LLVM Compiler Infrastructure
+ *
+ * This file is dual licensed under the MIT and the University of Illinois Open
+ * Source Licenses. See LICENSE.TXT for details.
+ *
+ */
+
+/* Returns: the product of a + ib and c + id */
+
+double _Complex
+__muldc3(double __a, double __b, double __c, double __d)
+{
+    double __ac = __a * __c;
+    double __bd = __b * __d;
+    double __ad = __a * __d;
+    double __bc = __b * __c;
+    double _Complex z;
+    __real__ z = __ac - __bd;
+    __imag__ z = __ad + __bc;
+    if (isnan(__real__ z) && isnan(__imag__ z))
+    {
+        int __recalc = 0;
+        if (isinf(__a) || isinf(__b))
+        {
+            __a = copysign(isinf(__a) ? 1 : 0, __a);
+            __b = copysign(isinf(__b) ? 1 : 0, __b);
+            if (isnan(__c))
+                __c = copysign(0, __c);
+            if (isnan(__d))
+                __d = copysign(0, __d);
+            __recalc = 1;
+        }
+        if (isinf(__c) || isinf(__d))
+        {
+            __c = copysign(isinf(__c) ? 1 : 0, __c);
+            __d = copysign(isinf(__d) ? 1 : 0, __d);
+            if (isnan(__a))
+                __a = copysign(0, __a);
+            if (isnan(__b))
+                __b = copysign(0, __b);
+            __recalc = 1;
+        }
+        if (!__recalc && (isinf(__ac) || isinf(__bd) ||
+                          isinf(__ad) || isinf(__bc)))
+        {
+            if (isnan(__a))
+                __a = copysign(0, __a);
+            if (isnan(__b))
+                __b = copysign(0, __b);
+            if (isnan(__c))
+                __c = copysign(0, __c);
+            if (isnan(__d))
+                __d = copysign(0, __d);
+            __recalc = 1;
+        }
+        if (__recalc)
+        {
+            __real__ z = INFINITY * (__a * __c - __b * __d);
+            __imag__ z = INFINITY * (__a * __d + __b * __c);
+        }
+    }
+    return z;
+}
+
+float _Complex
+__mulsc3(float __a, float __b, float __c, float __d)
+{
+    float __ac = __a * __c;
+    float __bd = __b * __d;
+    float __ad = __a * __d;
+    float __bc = __b * __c;
+    float _Complex z;
+    __real__ z = __ac - __bd;
+    __imag__ z = __ad + __bc;
+    if (isnan(__real__ z) && isnan(__imag__ z))
+    {
+        int __recalc = 0;
+        if (isinf(__a) || isinf(__b))
+        {
+            __a = copysignf(isinf(__a) ? 1 : 0, __a);
+            __b = copysignf(isinf(__b) ? 1 : 0, __b);
+            if (isnan(__c))
+                __c = copysignf(0, __c);
+            if (isnan(__d))
+                __d = copysignf(0, __d);
+            __recalc = 1;
+        }
+        if (isinf(__c) || isinf(__d))
+        {
+            __c = copysignf(isinf(__c) ? 1 : 0, __c);
+            __d = copysignf(isinf(__d) ? 1 : 0, __d);
+            if (isnan(__a))
+                __a = copysignf(0, __a);
+            if (isnan(__b))
+                __b = copysignf(0, __b);
+            __recalc = 1;
+        }
+        if (!__recalc && (isinf(__ac) || isinf(__bd) ||
+                          isinf(__ad) || isinf(__bc)))
+        {
+            if (isnan(__a))
+                __a = copysignf(0, __a);
+            if (isnan(__b))
+                __b = copysignf(0, __b);
+            if (isnan(__c))
+                __c = copysignf(0, __c);
+            if (isnan(__d))
+                __d = copysignf(0, __d);
+            __recalc = 1;
+        }
+        if (__recalc)
+        {
+            __real__ z = INFINITY * (__a * __c - __b * __d);
+            __imag__ z = INFINITY * (__a * __d + __b * __c);
+        }
+    }
+    return z;
+}
+
+double _Complex
+__divdc3(double __a, double __b, double __c, double __d)
+{
+    int __ilogbw = 0;
+    double __logbw = logb(fmax(fabs(__c), fabs(__d)));
+    double __denom = __c * __c + __d * __d;
+    double _Complex z;
+    if (isfinite(__logbw))
+    {
+        __ilogbw = (int)__logbw;
+        __c = scalbn(__c, -__ilogbw);
+        __d = scalbn(__d, -__ilogbw);
+    }
+    __real__ z = scalbn((__a * __c + __b * __d) / __denom, -__ilogbw);
+    __imag__ z = scalbn((__b * __c - __a * __d) / __denom, -__ilogbw);
+    if (isnan(__real__ z) && isnan(__imag__ z))
+    {
+        if ((__denom == 0.0) && (!isnan(__a) || !isnan(__b)))
+        {
+            __real__ z = copysign(INFINITY, __c) * __a;
+            __imag__ z = copysign(INFINITY, __c) * __b;
+        }
+        else if ((isinf(__a) || isinf(__b)) && isfinite(__c) && isfinite(__d))
+        {
+            __a = copysign(isinf(__a) ? 1.0 : 0.0, __a);
+            __b = copysign(isinf(__b) ? 1.0 : 0.0, __b);
+            __real__ z = INFINITY * (__a * __c + __b * __d);
+            __imag__ z = INFINITY * (__b * __c - __a * __d);
+        }
+        else if (isinf(__logbw) && __logbw > 0.0 && isfinite(__a) && isfinite(__b))
+        {
+            __c = copysign(isinf(__c) ? 1.0 : 0.0, __c);
+            __d = copysign(isinf(__d) ? 1.0 : 0.0, __d);
+            __real__ z = 0.0 * (__a * __c + __b * __d);
+            __imag__ z = 0.0 * (__b * __c - __a * __d);
+        }
+    }
+    return z;
+}
+
+
+float _Complex
+__divsc3(float __a, float __b, float __c, float __d)
+{
+    int __ilogbw = 0;
+    float __logbw = logbf(fmaxf(fabsf(__c), fabsf(__d)));
+    float __denom = __c * __c + __d * __d;
+    float _Complex z;
+    if (isfinite(__logbw))
+    {
+        __ilogbw = (int)__logbw;
+        __c = scalbnf(__c, -__ilogbw);
+        __d = scalbnf(__d, -__ilogbw);
+    }
+    __real__ z = scalbnf((__a * __c + __b * __d) / __denom, -__ilogbw);
+    __imag__ z = scalbnf((__b * __c - __a * __d) / __denom, -__ilogbw);
+    if (isnan(__real__ z) && isnan(__imag__ z))
+    {
+        if ((__denom == 0) && (!isnan(__a) || !isnan(__b)))
+        {
+            __real__ z = copysignf(INFINITY, __c) * __a;
+            __imag__ z = copysignf(INFINITY, __c) * __b;
+        }
+        else if ((isinf(__a) || isinf(__b)) && isfinite(__c) && isfinite(__d))
+        {
+            __a = copysignf(isinf(__a) ? 1 : 0, __a);
+            __b = copysignf(isinf(__b) ? 1 : 0, __b);
+            __real__ z = INFINITY * (__a * __c + __b * __d);
+            __imag__ z = INFINITY * (__b * __c - __a * __d);
+        }
+        else if (isinf(__logbw) && __logbw > 0 && isfinite(__a) && isfinite(__b))
+        {
+            __c = copysignf(isinf(__c) ? 1 : 0, __c);
+            __d = copysignf(isinf(__d) ? 1 : 0, __d);
+            __real__ z = 0 * (__a * __c + __b * __d);
+            __imag__ z = 0 * (__b * __c - __a * __d);
+        }
+    }
+    return z;
+}
+
+#endif
+
+#endif
+
+#else 
+
 /*
 rtai/libm/libm.c - module wrapper for SunSoft/FreeBSD/MacOX/uclibc libm
 RTAI - Real-Time Application Interface
@@ -114,3 +641,5 @@ EXPORT_SYMBOL(yn);
 EXPORT_SYMBOL(libm_errno);
 #endif /* CONFIG_RTAI_MATH_C99 */
 #endif /* CONFIG_KBUILD */
+
+#endif
