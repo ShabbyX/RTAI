de529b0c0daabc70a183f3211edc67d9d4e24cc9
Jeff Epler <jepler@unpythonic.net>
2014-08-23 08:40:11 -0500

    Remove cut & paste copies of non-free code
    .. rather than using this implementation of isinf with dubious
    provenance, we can depend on __builtin_isinf, which compiles to
    an inline instruction sequence on i386, x86_64, and armhf.

--- base/math/mathP.h
+++ base/math/mathP.h
@@ -225,4 +225,9 @@ extern float __kernel_cosf __P((float,float));
 extern float __kernel_tanf __P((float,float,int));
 extern int   __kernel_rem_pio2f __P((float*,float*,int,int,int,const int*));
 
+#ifdef isnan
+#undef isnan
+#endif
+#define isnan(x) __builtin_isnan(x)
+
 #endif /* _MATH_PRIVATE_H_ */
